476e2a2f3facd974ee3299e2216e24167ec2ee0a
wesley1710 <wesleyferreira1710@gmail.com>
2014-08-06 21:25:07 -0300

    Atualizações de Melhorias

--- js/likebar.js
+++ js/likebar.js
@@ -1,18 +1,23 @@
 (function($){
     $.fn.likeBar = function(settings){
+        if(typeof(settings) == "string"){
+            settings = {url: settings}
+        }
         var config = {
             url: "",
-            css: "online"
+            css: "online",
+            width: "100%",
         };
         if (settings){$.extend(config, settings);}
-		
+        
 		var urlAvatar = "https://www.facebook.com/plugins/likebox.php?href=" + config.url + "&amp;width&amp;height=300&amp;colorscheme=light&amp;show_faces=false&amp;header=false&amp;stream=false&amp;show_border=false";
 		var urlFaces = "https://www.facebook.com/plugins/facepile.php?app_id&amp;href=" + config.url + "&amp;action&amp;width&amp;height&amp;max_rows=2&amp;colorscheme=light&amp;size=large&amp;show_count=false";
 
         return this.each(function(){
+            $(this).css("width", config.width).removeClass("likebarcontent").addClass("likebarcontent");
 			var iframe1 = '<iframe class="fbAvatar" src="' + urlAvatar + '" scrolling="no" frameborder="0" allowTransparency="true"></iframe>';
         	$(this).append(iframe1);
-			$(".fbAvatar").css("height", $("._8o._8r.lfloat._ohe").width());
+            
 			var iframe2 = '\
 			<div class="parentFbFaces">\
 				<iframe class="fbFriendFaces" id="fbFriendFaces" src="' + urlFaces + '" scrolling="no" frameborder="0" allowTransparency="true"></iframe>\
@@ -25,5 +30,5 @@
 })(jQuery);
 
 $(".likebarcontent").each(function(){
-	$(this).likeBar($(this).attr('data-url'));
-})
+	$(this).likeBar($(this).attr("data-url"));
+});
